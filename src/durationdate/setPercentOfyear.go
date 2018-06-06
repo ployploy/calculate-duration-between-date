19f7a59d148520e027be2a4955e7a40c6e8439c3
gunner1886 <johndoe@example.com>
2018-06-06 15:43:49 +0700

    [Delete]:function mockup after test production

--- src/durationdate/setPercentOfyear.go
+++
@@ -1,9 +0,0 @@
-package durationdate
-
-import "strconv"
-
-func TransferToPercentOfYear(day int) string {
-	var ResultPercentOfYear float64 = (float64(day) / 365) * 100
-	percentofyear := strconv.FormatFloat(ResultPercentOfYear, 'f', 2, 64)
-	return percentofyear
-}
