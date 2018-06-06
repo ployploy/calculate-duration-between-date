5d18964aacc2a85e78bc1782f8156004e8968d34
CAPTAINAMERICA <captainamerica@CAPTAINAMERICAs-Mac-mini.local>
2018-06-06 13:43:14 +0700

    [Create] ฟังก์ชัน setDay เพื่อสำหรับคำนวณระยะห่างของวันเริ่มต้นและวันสิ้นสุด

---
+++ src/durationdate/setDay.go
@@ -0,0 +1,19 @@
+package durationdate
+
+import (
+	"strconv"
+	"time"
+)
+
+func SetDay(startday int, startmonth int, startyear int, endday int, endmonth int, endyear int) string {
+	startDate := time.Date(startyear, time.Month(startmonth), startday, 0, 0, 0, 0, time.UTC)
+	endDate := time.Date(endyear, time.Month(endmonth), endday, 0, 0, 0, 0, time.UTC)
+
+	diff := endDate.Sub(startDate)
+	diffDay := ((diff.Hours()) + 24) / 24
+
+	Day := strconv.FormatFloat(diffDay, 'f', 0, 64)
+
+	return Day
+
+}
