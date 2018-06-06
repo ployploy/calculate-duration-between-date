e61e98e5181f4a6d23961ce77c10a807391e19e6
gunner1886 <johndoe@example.com>
2018-06-06 15:34:45 +0700

    [Create]:function setPercentOfYear_test.go

--- src/durationdate/setPercentOfYear_test.go
+++ src/durationdate/setPercentOfYear_test.go
@@ -5,8 +5,8 @@ import (
 )
 
 func Test_setPercentOfYear_input_152_Should_be_41_dot_46(t *testing.T) {
-	day := 152
-	resultPercentOfYear := TransferToPercentOfYear(day)
+	days := 152
+	resultPercentOfYear := TransferToPercentOfYear(days)
 	expected := "41.64"
 	if resultPercentOfYear != expected {
 		t.Errorf("expected %s but %s", expected, resultPercentOfYear)
