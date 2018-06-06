15306ad27334659ea2e4eec4153d7e6d88ee2018
CAPTAINAMERICA <captainamerica@CAPTAINAMERICAs-Mac-mini.local>
2018-06-06 14:26:54 +0700

    [Edit]:name function

--- src/durationdate/setMinute_test.go
+++ src/durationdate/setMinute_test.go
@@ -6,7 +6,7 @@ import (
 
 func Test_setMinute_input_152_Should_be_218880(t *testing.T) {
 	day := 152
-	resultMinute := setMinute(day)
+	resultMinute := TransferToMinute(day)
 	expected := "218880"
 	if resultMinute != expected {
 		t.Errorf("expected %s but %s", expected, resultMinute)
