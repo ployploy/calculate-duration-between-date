163835bfcb046e335971105f2678614a3e562563
gunner1886 <johndoe@example.com>
2018-06-06 14:24:50 +0700

    [Create]: sethours test file

---
+++ src/durationdate/setHours_test.go
@@ -0,0 +1,15 @@
+package durationdate
+
+import (
+	"testing"
+)
+
+func Test_setHours_input_152_Should_be_3648(t *testing.T) {
+	day := 152
+	resultHours := setHours(day)
+	expected := "3648"
+	if resultHours != expected {
+		t.Errorf("expected %s but %s", expected, resultHours)
+	}
+
+}
