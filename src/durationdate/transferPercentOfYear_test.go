533061549e92dc41b4ade10727c09f72181e9d90
gunner1886 <johndoe@example.com>
2018-06-06 15:45:05 +0700

    [Edit]:rename function test case

--- src/durationdate/transferPercentOfYear_test.go
+++ src/durationdate/transferPercentOfYear_test.go
@@ -5,8 +5,8 @@ import (
 )
 
 func Test_setPercentOfYear_input_152_Should_be_41_dot_46(t *testing.T) {
-	days := 152
-	resultPercentOfYear := TransferToPercentOfYear(days)
+	day := 152
+	resultPercentOfYear := TransferToPercentOfYear(day)
 	expected := "41.64"
 	if resultPercentOfYear != expected {
 		t.Errorf("expected %s but %s", expected, resultPercentOfYear)
