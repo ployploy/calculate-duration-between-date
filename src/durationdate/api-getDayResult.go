fc067efe0afba8d41f638d9728695b25daeee2cf
blackwidow <blackwidow@blackwidows-Mac-mini.local>
2018-06-06 15:56:20 +0700

    [Edit]:Fucntion Duration

--- src/durationdate/api-getDayResult.go
+++ src/durationdate/api-getDayResult.go
@@ -4,7 +4,17 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"strings"
 )
+
+type dateInput struct {
+	DayFrom   string `json:"dayfrom"`
+	MonthFrom string `json:"monthfrom"`
+	YearFrom  string `json:"yearfrom"`
+	DayTo     string `json:"dayto"`
+	MonthTo   string `json:"monthto"`
+	YearTo    string `json:"yearto"`
+}
 
 type dayResult struct {
 	DayNameFrom   string `json:"daynamefrom"`
@@ -30,27 +40,41 @@ type statusCode struct {
 	Status string `json:"status"`
 }
 
-func GetDayResult(w http.ResponseWriter, r *http.Request) {
-	dayResultStruct := dayResult{
-		DayNameFrom:   "Thursday",
-		DayFrom:       "4",
-		MonthNameFrom: "January",
-		YearFrom:      "2018",
-		DayNameTo:     "Monday",
-		DayTo:         "4",
-		MonthNameTo:   "June",
-		YearTo:        "2018",
-		ResultDay:     "152",
-		ResultYear:    "0",
-		ResultMonth:   "5",
-		ResultOverDay: "1",
-		Second:        "13,132,800",
-		Minute:        "218,880",
-		Hours:         "3,648",
-		Week:          "21",
-		DayOverOfWeek: "5",
-		Percent:       "41.64",
-	}
+var dayResultStruct = dayResult{
+	DayNameFrom:   "Thursday",
+	DayFrom:       "4",
+	MonthNameFrom: "January",
+	YearFrom:      "2018",
+	DayNameTo:     "Monday",
+	DayTo:         "4",
+	MonthNameTo:   "June",
+	YearTo:        "2018",
+	ResultDay:     "152",
+	ResultYear:    "0",
+	ResultMonth:   "5",
+	ResultOverDay: "1",
+	Second:        "13,132,800",
+	Minute:        "218,880",
+	Hours:         "3,648",
+	Week:          "21",
+	DayOverOfWeek: "5",
+	Percent:       "41.64",
+}
+
+func GetStartDateAndEndDate(r *http.Request) dateInput {
+	startDate := r.URL.Query().Get("startdate")
+	endDate := r.URL.Query().Get("enddate")
+
+	s := strings.Split(startDate, "/")
+	e := strings.Split(endDate, "/")
+
+	startEndDate := dateInput{DayFrom: s[0], MonthFrom: s[1], YearFrom: s[2], DayTo: e[0], MonthTo: e[1], YearTo: e[2]}
+
+	return startEndDate
+}
+
+func Duration(w http.ResponseWriter, r *http.Request) {
+	// date := GetStartDateAndEndDate(r)
 	jsonMarshallDayResultStruct, err := json.Marshal(dayResultStruct)
 	if err != nil {
 		log.Println(err)
