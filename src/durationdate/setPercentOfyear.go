4748a1d8b70c54e1cb6ab4bd996a3cf2ffc63fd5
gunner1886 <johndoe@example.com>
2018-06-06 15:32:52 +0700

    [Create]:function setPercentOfyear.go

--- src/durationdate/setPercentOfyear.go
+++ src/durationdate/setPercentOfyear.go
@@ -4,6 +4,6 @@ import "strconv"
 
 func TransferToPercentOfYear(day int) string {
 	var ResultPercentOfYear float64 = (float64(day) / 365) * 100
-	s64 := strconv.FormatFloat(ResultPercentOfYear, 'f', 2, 64)
-	return s64
+	percentofyear := strconv.FormatFloat(ResultPercentOfYear, 'f', 2, 64)
+	return percentofyear
 }
