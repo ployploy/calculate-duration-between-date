624fd00d6b987694f382162d67bd4e89fd3c5b11
CAPTAINAMERICA <captainamerica@CAPTAINAMERICAs-Mac-mini.local>
2018-06-06 14:36:24 +0700

    [Edit]:name function

--- src/durationdate/setHours_test.go
+++ src/durationdate/setHours_test.go
@@ -6,7 +6,7 @@ import (
 
 func Test_setHours_input_152_Should_be_3648(t *testing.T) {
 	day := 152
-	resultHours := setHours(day)
+	resultHours := TransferToHour(day)
 	expected := "3648"
 	if resultHours != expected {
 		t.Errorf("expected %s but %s", expected, resultHours)
